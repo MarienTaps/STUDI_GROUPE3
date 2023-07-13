016c141430bfcf30861c160ae6ec060025c762ac
Fleurdel34 <betdevelweb0953@gmail.com>
2023-07-13 21:00:09 +0200

    insertion route et component experience

--- pharmacyapp/src/app/app-routing.module.ts
+++ pharmacyapp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil/accueil.component';
 import { ContactComponent } from './contact/contact/contact.component';
+import { EsperienceComponent} from './esperience/esperience/esperience.component'
 import { ProduitsComponent } from './produits/produits/produits.component';
 import { PrestationsComponent } from './prestations/prestations/prestations.component';
 import { MarquesComponent } from './marques/marques/marques.component';
@@ -9,6 +10,7 @@ import { MarquesComponent } from './marques/marques/marques.component';
 const routes: Routes = [
   {path: 'accueil', title: 'Accueil', component: AccueilComponent },
   {path: 'contact', title: 'Contact', component: ContactComponent },
+  {path: 'esperience', title: 'Experience', component: EsperienceComponent },
   {path: 'produits', title: 'Produits', component: ProduitsComponent },
   {path: 'prestations', title: 'Prestations', component: PrestationsComponent },
   {path: 'marques', title: 'Marques', component: MarquesComponent },
