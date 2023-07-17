17c97dea531fdf6dac708e1124c26a4597766a4d
MarienTaps <marientaps@gmail.com>
2023-07-17 21:01:42 +0200

    produit_V2

--- pharmacyapp/src/app/app-routing.module.ts
+++ pharmacyapp/src/app/app-routing.module.ts
@@ -6,6 +6,14 @@ import { ProduitsComponent } from './produits/produits/produits.component';
 import { PrestationsComponent } from './prestations/prestations/prestations.component';
 import { MarquesComponent } from './marques/marques/marques.component';
 import { ContactformComponent } from './contactform/contactform.component';
+import { PreventionComponent } from './produits/produits/prevention/prevention.component';
+import { ComplementsComponent } from './produits/produits/complements/complements.component';
+import { SpagyrieComponent } from './produits/produits/spagyrie/spagyrie.component';
+import { VenteComponent } from './produits/produits/vente/vente.component';
+import { MaquillageComponent } from './produits/produits/maquillage/maquillage.component';
+import { HomeopathieComponent } from './produits/produits/homeopathie/homeopathie.component';
+import { PhytotherapieComponent } from './produits/produits/phytotherapie/phytotherapie.component';
+import { EsperienceComponent } from './esperience/esperience/esperience.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/accueil', pathMatch: 'full'},
@@ -15,6 +23,14 @@ const routes: Routes = [
   {path: 'prestations', title: 'Prestations', component: PrestationsComponent },
   {path: 'marques', title: 'Marques', component: MarquesComponent },
   {path: 'contactform', title: 'Inscription', component: ContactformComponent },
+  {path: 'esperience', title:'Experience & Histoire', component: EsperienceComponent },
+  {path: 'prevention', title: 'Prevention', component:PreventionComponent},
+  {path: 'complement', title: 'Complement', component :ComplementsComponent },
+  {path: 'spagyrie', title: 'Spagyrie', component :SpagyrieComponent},
+  {path: 'vente', title: 'Vente libre', component :VenteComponent },
+  {path: 'maquillage', title: 'Maquillages', component :MaquillageComponent },
+  {path: 'homeopathie', title: 'Homeopathie', component :HomeopathieComponent },
+  {path: 'phytotherapie', title: 'Phytotherapie', component :PhytotherapieComponent }
 ];
 
 @NgModule({
